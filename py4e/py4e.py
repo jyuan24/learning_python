95dd42ab769a7153b1d45519bebdf5766a0b08e9
unknown <joseph.yuan24@gmail.com>
2020-07-16 14:29:05 -0700

    updated

--- py4e/py4e.py
+++ py4e/py4e.py
@@ -335,3 +335,32 @@
 #     print('Name', item['name'])
 #     print('Id', item['id'])
 #     print('Attribute', item['x'])
+
+
+
+
+# https://api.github.com/users/jyuan24
+import urllib.request, urllib.parse, urllib.error
+import json
+serviceurl = 'https://api.github.com/users/'
+while True:
+    username = input('Enter username: ')
+    if len(username) < 1: break
+    url = serviceurl + urllib.parse.urlencode({'username': username}) # this will result in a 'username=jyuan24' in address...not ideal...
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+try:
+    js = json.loads(data)
+except:
+    js = None
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('==== Failure To Retrieve ====')
+    print(data)
+    continue
+lat = js['results'][0]['geometry']['location']['lat']
+lng = js['results'][0]['geometry']['location']['lng']
+print('lat', lat, 'lng', lng)
+location = js['results'][0]['formatted_address']
+print(location)
