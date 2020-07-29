8b0566481b9b3dd766002bc2398bd98e870eb3f9
unknown <joseph.yuan24@gmail.com>
2020-07-29 15:10:34 -0700

    finished one to many relationships

--- py4e/py4e.py
+++ py4e/py4e.py
@@ -599,5 +599,91 @@
 
 
 
+# # tracks.py
+# import xml.etree.ElementTree as ET # reads xml
+# import sqlite3 # reads sqlite db
+
+# conn = sqlite3.connect('trackdb.sqlite') # creates db if doesn't already exist
+# cur = conn.cursor()
+
+# # Make some fresh tables using executescript()
+# cur.executescript('''
+# DROP TABLE IF EXISTS Artist;
+# DROP TABLE IF EXISTS Album;
+# DROP TABLE IF EXISTS Track;
+
+# CREATE TABLE Artist (
+#     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+#     name    TEXT UNIQUE
+# );
+
+# CREATE TABLE Album (
+#     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+#     artist_id  INTEGER,
+#     title   TEXT UNIQUE
+# );
+
+# CREATE TABLE Track (
+#     id  INTEGER NOT NULL PRIMARY KEY 
+#         AUTOINCREMENT UNIQUE,
+#     title TEXT  UNIQUE,
+#     album_id  INTEGER,
+#     len INTEGER, rating INTEGER, count INTEGER
+# );
+# ''') # create these tables fresh if they dne
+
+
+# fname = input('Enter file name: ')
+# if ( len(fname) < 1 ) : fname = 'Library.xml' # apples xml...as mentioned would be a lot better in json
+
+# # <key>Track ID</key><integer>369</integer>
+# # <key>Name</key><string>Another One Bites The Dust</string>
+# # <key>Artist</key><string>Queen</string>
+# def lookup(d, key): # key is second due to how this xml was designed
+#     found = False
+#     for child in d:
+#         if found : return child.text
+#         if child.tag == 'key' and child.text == key :
+#             found = True
+#     return None
+
+# stuff = ET.parse(fname)
+# all = stuff.findall('dict/dict/dict') # goes down to 3rd level of dict which contains track info
+# print('Dict count:', len(all))
+# for entry in all:
+#     if ( lookup(entry, 'Track ID') is None ) : continue
+
+#     name = lookup(entry, 'Name')
+#     artist = lookup(entry, 'Artist')
+#     album = lookup(entry, 'Album')
+#     count = lookup(entry, 'Play Count')
+#     rating = lookup(entry, 'Rating')
+#     length = lookup(entry, 'Total Time')
+
+#     if name is None or artist is None or album is None : 
+#         continue
+
+#     print(name, artist, album, count, rating, length)
+
+#     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+#         VALUES ( ? )''', ( artist, ) ) # because artist name is unique, it will blow up if inserting duplicate artist name, therefore insert or ignore
+#     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, )) # select above artist id
+#     artist_id = cur.fetchone()[0]
+
+#     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+#         VALUES ( ?, ? )''', ( album, artist_id ) )
+#     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+#     album_id = cur.fetchone()[0]
+
+#     cur.execute('''INSERT OR REPLACE INTO Track
+#         (title, album_id, len, rating, count) 
+#         VALUES ( ?, ?, ?, ?, ? )''', # similar to above, except this replaces instead of ignores
+#         ( name, album_id, length, rating, count ) )
+
+#     conn.commit()
+
+
+
+
 
 
